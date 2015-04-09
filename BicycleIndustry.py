0f416bab272a11db9377b1a3c568f0d4d1985f24
Yih-Yoon Lee <yihyoon@gmail.com>
2015-04-08 21:30:59 -0500

    Create Basic Classes

--- BicycleIndustry.py
+++ BicycleIndustry.py
@@ -7,11 +7,10 @@ class Bicycle(object):
             'Madone': [150, 100],
             'Lexa': [400, 90],
             'Silque': [1000, 80],
-            'FX': [1500, 75],
-            'CrossRip': [4500, 60],
+            'FX': [1500, 70],
+            'CrossRip': [4500, 65],
             'Tandem': [8000, 55]
         }
-        self.bikeWeight = {'Madone'}
 
 
 class Shop(object):
@@ -28,14 +27,22 @@ class Shop(object):
         self.totalProfit = totalProfit
 
 
-class Customer(object):
-    def name(self, customerName):
+class Customer:
+    def __init__(self, customerName, availMoney, buy='Y'):
         self.customerName = customerName
-
-    def fund(self, availMoney):
         self.availMoney = availMoney
-
-    def buyBike(self, buy):
         self.buy = buy
+
+
+FriscoBikeMart = Shop()
+FriscoBikeMart.shopName = 'Fisco Bike\'s Mart'
+FriscoBikeMart.inventory = {'Madone': 5, 'Lexa': 3, 'Silque': 5, 'FX': 2, 'CrossRip': 1, 'Tandem': 1}
+
+Lee = Customer('Yih-Yoon Lee', 200, 'Y')
+Chia = Customer('Chia-Hwa Chang', 500, 'Y')
+Joseph = Customer('Joseph Tan', 1000, 'Y')
+
+
+
 
 
